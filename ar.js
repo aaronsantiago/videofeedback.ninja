6401eb9c5737d39f229b70eba9088410d4ad9c3a
Aaron Santiago <aaronsantiago@gmail.com>
2019-08-16 19:35:59 -0400

    revert ar.js, set up autopositioning

--- ar.js
+++ ar.js
@@ -6024,10 +6024,7 @@ ARjs.Source.prototype._initSourceWebcam = function(onReady, onError) {
                 var userMediaConstraints = {
 			audio: false,
 			video: {
-
-                //**** change facing of camera *****
-                // facingMode: 'environment',
-				facingMode: 'user',
+				facingMode: 'environment',
 				width: {
 					ideal: _this.parameters.sourceWidth,
 					// min: 1024,
